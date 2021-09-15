4eaec26111cbf20ef06f6d2bed9aaa422ec1153e
levisc8 <levisc8@gmail.com>
2021-09-15 21:21:06 +0200

    first pieces of dashboard

--- app.R
+++ app.R
@@ -1,82 +1,87 @@
 # shiny app to explore PADRINO interactively
 
-library(shiny)
-library(shinythemes)
+library(shinydashboard)
 library(rlang)
 library(tools)
 library(dplyr)
 library(RPadrino)
-library(markdown)
+
 
 
 pdb <- pdb_download(save = FALSE)
 
-.pretty_names <- function(x) {
+.pdb_2_pretty_names <- function(x) {
 
   temp <- gsub("_", " ", names(x))
 
   toTitleCase(temp)
 }
 
+.pretty_2_pdb_names <- function(x) {
 
-ui <- fluidPage(
+  temp <- gsub(" ", "_", x)
 
-  navbarPage(
-    "PADRINO Database", theme = shinytheme("lumen"),
-    tabPanel("PADRINO Overview", fluid = TRUE, icon = icon("atlas"),
-             fluidRow(
-               column(6,
-                      h4(p("Welcome to the PADRINO Shiny App!")),
-                      h5(p("PADRINO is an open access database of published Integral Projection Models (IPMs). The goal of this app is to allow you to explore the models contained in the database. Analysis capabilities are available in the `RPadrino` R package.")))
-             )
-    ),
-    tabPanel("Metadata", fluid = TRUE, icon = icon("seedling"),
-             titlePanel("PADRINO Metadata"),
-             fluidRow(
-               column(6,
-                      checkboxGroupInput(inputId  = "met_cols",
-                                         label    = "Metadata Options",
-                                         choices  = .pretty_names(pdb$Metadata),
-                                         selected = c("species_accepted",
-                                                      "tax_family",
-                                                      "tax_kingdom",
-                                                      "pub_year",
-                                                      "duration",
-                                                      "lat",
-                                                      "lon",
-                                                      "country",
-                                                      "eviction_used",
-                                                      "treatment",
-                                                      "has_dd"
-                                         )
-                      )
-
-               ),
-               column(6,
-                      selectizeInput(inputId = "sel_by",
-                                     label   = "Summary Options",
-                                     choices = .pretty_names(pdb$Metadata),
-                                     multiple = TRUE,
-                                     options  = list(placeholder = ' Metadata Column Names',
-                                                     onInitialize = I('function() {this.setValue(""); }')))
-
-                      )
-             )
-
-    )
-  )
-
-
-)
-
-
-server <- function(input, output, session) {
-
-  reactive({
-
-
-
-
-  })
+  tolower(temp)
 
 }
+
+
+ui <- shinyUI(
+  fluidPage(
+    dashboardPage(
+      skin = "green",
+
+      dashboardHeader(title = "Padrino Database"),
+      dashboardSidebar(
+        menuItem("Home", tabName = "home", icon = icon("home")),
+        menuItem("Metadata", tabName = "metadata", icon = icon("book"))
+      ),
+
+      dashboardBody(
+        tabItems(
+          tabItem(tabName = "home",
+                  includeMarkdown("www/home.md")),
+
+
+          tabItem(tabName = "metadata",
+                  fluidRow(
+                    tableOutput("sum_tab"),
+                    box(title = "Select Columns to Summarize Metadata by",
+
+                        selectizeInput(inputId = "gr_by",
+                                       label   = "Summarize By",
+                                       choices = names(pdb[[1]]),
+                                       multiple = TRUE)
+
+                    )
+                  )
+          )
+        )
+
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  output$sum_tab  <- renderTable({
+    gr_by <- syms(input$gr_by)
+
+    out_tab <- pdb$Metadata %>%
+      group_by(!!! gr_by) %>%
+      summarise(id = length(unique(ipm_id)),
+                spp = length(unique(species_accepted)),
+                pubs = length(unique(apa_citation))) %>%
+      setNames(c(names(.)[1:(length(names(.)) - 3)],
+                 "# of ipm_id's",
+                 "# of Species",
+                 "# of Publications"))
+
+    out_tab
+  })
+}
+
+
+
+shinyApp(ui = ui, server = server)
